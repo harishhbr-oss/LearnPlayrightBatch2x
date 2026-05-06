b8d06ac19803c162059e0b93024558d0ffc54db4
Harish Rao [CW] <harish.rao@trellix.com>
2026-05-06 13:15:57 +0530

    Initial commit: Add basic JavaScript files for Playwright learning

---
+++ chapter_01_Basics/02_JS_VerifySetup.js
@@ -0,0 +1,9 @@
+console.log(process.platform);
+console.log(process.arch);
+console.log(process.log);
+console.log(process.console);
+console.log("Node version is " + process.version);
+
+const now = new Date();
+console.log("todays date is" +  now);
+
---
+++ chapter_01_Basics/hello.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+let a = 5;
+
+for(let i=0; i<5; i++){
+    console.log("the no is " + i);
+}
